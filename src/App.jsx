343ed5fa86b60417fc72218ef4d1bbda8ee16984
MelvyN <94375834+Balakaboum@users.noreply.github.com>
2026-09-03 17:03:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
+        <Router basename="/couvreur">
           <ScrollToTop />
           <AuthenticatedApp />
         </Router>
